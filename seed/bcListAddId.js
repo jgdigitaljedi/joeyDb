bca74f2fe9d91cad28c3a0845203d1e8985215a1
Joey Gauthier <digitaljedi@outlook.com>
2019-06-15 06:32:21 -0500

    more work on getting xbox backward compatibility games populated with IGDB ID

--- seed/bcListAddId.js
+++ seed/bcListAddId.js
@@ -1,4 +1,5 @@
 (function () {
+  const chalk = require('chalk');
   const fs = require('fs');
   const path = require('path');
   const apicalypse = require('apicalypse').default;
@@ -20,31 +21,91 @@
       .search(name)
       .where(`platforms = [${platformId}]`)
       .request('/games')
-      .then(result => result.data);
+      .then(result => result.data)
+      .catch(error => {
+        console.log(chalk.red.bold('ERROR FETCHING GAME DATA FROM IGDB API!', error));
+        return [];
+      });
+  };
+
+  function formatGame(g) {
+    g.name = g.game ? removeOtherRegionNames(g.game) : g.name;
+    g.igdbId = null;
+    delete g.game;
+    return g;
   }
 
-  /** For testing purposes right now */
-  const halo = igdbLookup('Halo: Combat Evolved', 11).then(result => {
-    console.log('result', result);
-    return result;
-  });
-  console.log('halo', halo);
+  function stripString(str) {
+    return str.toLowerCase().trim().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '').replace(/ +(?= )/g, '');
+  }
+
+  function removeOtherRegionNames(name) {
+    const split = name.split('  ');
+    if (split.length === 1) {
+      return name;
+    } else {
+      return split.filter(s => {
+        return (!s.endsWith('(JP)') && !s.endsWith('(PAL)'));
+      })[0].trim();
+    }
+  }
+
+  function dealWithResults(result, file) {
+    return result.map((item, index) => {
+      const matched = item.map((ig, i) => {
+        const oldName = stripString(removeOtherRegionNames(file[index].name));
+        const igName = stripString(ig.name);
+        if (igName === oldName) {
+          return { igdbId: ig.id, name: ig.name, notes: file[index].notes };
+        } else if (i === item.length - 1) {
+          return null;
+        }
+      }).filter(g => g && g.hasOwnProperty('name'));
+      if (matched && matched.length) {
+        return matched[0];
+      } else {
+        console.log(chalk.yellow.bold(`NO EXACT MATCH ON ${file[index].name}`));
+        return file[index];
+      }
+    });
+  }
 
   const xtxts = xboxToXboxThreeSixty.map(g => {
-    g.igdbId = null;
-    return g;
+    const game = formatGame(g);
+    return igdbLookup(game.name, 11);
   });
-  fs.writeFileSync(path.join(__dirname, '../src/static/xboxToXboxThreeSixty.json'), JSON.stringify(xtxts, null, 4));
+  Promise.all(xtxts).then(result => {
+    if (result && result.length) {
+      const final = dealWithResults(result, xboxToXboxThreeSixty);
+      fs.writeFileSync(path.join(__dirname, '../src/static/xboxToXboxThreeSixty.json'), JSON.stringify(final, null, 4));
+    } else {
+      console.log(chalk.red.bold('Error writing Xbox to Xbox 360 list!'));
+    }
+  });
 
-  const xtstxbo = xbox360ToXboxOne.map(g => {
-    g.igdbId = null;
-    return g;
-  });
-  fs.writeFileSync(path.join(__dirname, '../src/static/xbox360ToXboxOne.json'), JSON.stringify(xtstxbo, null, 4));
+  // const xtstxbo = xbox360ToXboxOne.map(g => {
+  //   const game = formatGame(g);
+  //   return igdbLookup(game.name, 12);
+  // });
+  // Promise.all(xtstxbo).then(result => {
+  //   if (result && result.length) {
+  //     const final = dealWithResults(result, xbox360ToXboxOne);
+  //     fs.writeFileSync(path.join(__dirname, '../src/static/xbox360ToXboxOne.json'), JSON.stringify(final, null, 4));
+  //   } else {
+  //     console.log(chalk.red.bold('Error writing Xbox 360 to Xbox One list!'));
+  //   }
+  // });
 
-  const xtxbo = xboxToXboxOne.map(g => {
-    g.igdbId = null;
-    return g;
-  });
-  fs.writeFileSync(path.join(__dirname, '../src/static/xboxToXboxOne.json'), JSON.stringify(xtxbo, null, 4));
+  // const xtbo = xboxToXboxOne.map(g => {
+  //   const game = formatGame(g);
+  //   return igdbLookup(game.name, 11);
+  // });
+  // Promise.all(xtbo).then(result => {
+  //   if (result && result.length) {
+  //     const final = dealWithResults(result, xboxToXboxOne);
+  //     fs.writeFileSync(path.join(__dirname, '../src/static/xboxToXboxOne.json'), JSON.stringify(final, null, 4));
+  //   } else {
+  //     console.log(chalk.red.bold('Error writing Xbox to Xbox One list!'));
+  //   }
+  // });
 })();
